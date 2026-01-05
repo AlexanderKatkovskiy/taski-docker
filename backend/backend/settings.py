2e67d3746c826b8b8f8aa3a82979c554d9f8e6e0
Alexander Katkovskiy <alex-pe96@yandex.ru>
2026-01-05 17:16:30 +0300

    замена домена

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.164.190', 'https://ddt.hopto.org/']
+ALLOWED_HOSTS = ['localhost', '89.169.164.190', 'https://taski.myddns.me/']
 
 
 # Application definition
